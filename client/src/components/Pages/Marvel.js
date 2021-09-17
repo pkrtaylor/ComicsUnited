cb67c32fb447aaa964c2db0da277c603c809e241
Pierce Taylor <piercetaylor98@gmail.com>
2021-09-17 14:10:07 -0400

    update

--- client/src/components/Pages/Marvel.js
+++ client/src/components/Pages/Marvel.js
@@ -1,32 +1,32 @@
-import React, { useContext} from 'react'
-import {Link} from 'react-router-dom'
-import {CharContext} from '../../context/CharContext'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { CharContext } from '../../context/CharContext'
 
 const Marvel = () => {
 
 
-   
-    const {setName} = useContext(CharContext)
 
-  
+    const { setName } = useContext(CharContext)
+
+
     return (
-       
+
         <div className='hero-container'>
             <video src='/videos/CapAm.mp4' autoPlay loop muted />
             <form className=''>
-                <input 
-                type="text" 
-                placeholder=" Name" 
-                name="name"  
-                onChange={(event) => {
-                    setName(event.target.value)
-                }} />
-                
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    name="name"
+                    onChange={(event) => {
+                        setName(event.target.value)
+                    }} />
+
                 <Link to='/results'><input type="submit" /> </Link>
-              
+
             </form>
         </div>
-   
+
     )
 }
 
